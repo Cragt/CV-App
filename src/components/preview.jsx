ebb41b6023d36fe92513281fcc8d5b7e16c72eaf
Cragt <cragt369@gmail.com>
2024-07-25 02:58:59 -0500

    Add classes for component styling

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -11,21 +11,43 @@ export default function Preview({ formData }) {
     achievement,
     school,
     degree,
+    major,
   } = formData;
 
   return (
     <div>
-      <p>{fullName}</p>
-      <p>{email}</p>
-      <p>{number}</p>
-      <p>{title}</p>
-      <p>{company}</p>
-      <p>{startDate}</p>
-      <p>{endDate}</p>
-      <p>{responsible}</p>
-      <p>{achievement}</p>
-      <p>{school}</p>
-      <p>{degree}</p>
+      <h1>{fullName}</h1>
+      <div
+        className="email-number"
+        style={{
+          backgroundColor:
+            formData.email || formData.number ? "orange" : "transparent",
+        }}
+      >
+        <p>{number}</p>
+        <p>{email}</p>
+      </div>
+      <div className="work-history">
+        <h2>Work History</h2>
+        <h3>Title</h3>
+        <p>{title}</p>
+        <h3>Company</h3>
+        <p>{company}</p>
+        <p>
+          {startDate} to {endDate}
+        </p>
+        <h3>Responsibilities</h3>
+        <p>{responsible}</p>
+        <h3>Notable Achievements</h3>
+        <p>{achievement}</p>
+      </div>
+      <div className="education">
+        <h2>Education</h2>
+        <p>
+          {degree} degree in {major}
+        </p>
+        <p>{school}</p>
+      </div>
     </div>
   );
 }
